ce36d77d2570ba61b6f6997550269f56a645c3a9
unknown <mathewmendoza0512@gmail.com>
2022-07-17 09:44:32 +0800

    data model added

---
+++ backend/model/user.js
@@ -0,0 +1,62 @@
+export const user = [
+    {
+        username: "eric",
+        email: "",
+        phone: "",
+        password: "*****",
+        reservation: [
+            {
+                accomodationName: "",
+                status: "pending",
+            },
+        ],
+    },
+    {
+        username: "jay",
+        email: "",
+        phone: "",
+        password: "*****",
+        reservation: [
+            {
+                accomodationName: "",
+                status: "pending",
+            },
+        ],
+    },
+    {
+        username: "ally",
+        email: "",
+        phone: "",
+        password: "*****",
+        reservation: [
+            {
+                accomodationName: "",
+                status: "pending",
+            },
+        ],
+    },
+    {
+        username: "mat",
+        email: "",
+        phone: "",
+        password: "*****",
+        reservation: [
+            {
+                accomodationName: "",
+                status: "pending",
+            },
+        ],
+    },
+    {
+        username: "vanessa",
+        email: "",
+        phone: "",
+        password: "*****",
+        reservation: [
+            {
+                accomodationName: "",
+                status: "pending",
+            },
+        ],
+    },
+];
